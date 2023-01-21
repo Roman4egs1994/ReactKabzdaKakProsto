a1ff10fe34f229685291e8f066d22342884ae2f2
Roman <R.rybkin94@gmail.com>
2023-01-21 17:50:27 +0400

    added Select castom

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Select} from "./Select";
+import {ComponentMeta} from "@storybook/react";
+import {action, actions} from "@storybook/addon-actions";
 
 
+export default {
+    title: 'Select',
+    component: Select
+};
 
 
-export const Select = () => {
+export const BaseExample = () => {
+    const [value, setValue] = useState('2');
+
     return (
         <>
-            <select>
-
-            </select>
+            <Select
+                value={value}
+                onChange={setValue}
+                items={[
+                    {value: "1", title: "Minsk"},
+                    {value: "2", title: "UlsK"},
+                    {value: "3", title: "Kiev"}
+                ]}
+            />
         </>
-    );
-};
+    )
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState(null);
+
+    return (
+        <>
+            <Select
+                value={value}
+                onChange={setValue}
+                items={[
+                    {value: "1", title: "Minsk"},
+                    {value: "2", title: "UlsK"},
+                    {value: "3", title: "Kiev"}
+                ]}
+            />
+        </>
+    )
+}
 
 
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,29 +1,89 @@
-import React from 'react';
-
-
+import React, {KeyboardEvent, useEffect, useState} from 'react';
+import style from "./Select.module.css"
 
 
 type ItemType = {
-    title:string
+    title: string
     value: any
 }
 
 type SelectPropsType = {
-    value: any
-    onChange: (value:any)=> void
-    items:ItemType[]
+    value?: any
+    onChange: (value: any) => void
+    items: ItemType[]
 }
 
 
-export const Select = (props:SelectPropsType) => {
+export const Select = (props: SelectPropsType) => {
+    const [active, setActive] = useState<boolean | string>(false)
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
+
+    const toggleItems = () => {
+        setActive(!active)
+    }
+    const onItemClick = (value: any) => {
+        props.onChange(value);
+        toggleItems()
+    }
+
+    const selectedItem = props.items.find(i => i.value === props.value)
+    const hoveredItem = props.items.find(i => i.value === hoveredElementValue)
+
+
+    useEffect(()=>{
+        setHoveredElementValue(props.value)
+    },[props.value])
+
+
+
+    const onKeyUp = (e:KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredElementValue) {
+                    const pretendentElement = e.key === "ArrowDown"
+                        ? props.items[i + 1]
+                        : props.items[i - 1]
+                    if (pretendentElement) {
+                        props.onChange(pretendentElement.value);
+                        return
+                    } else {
+
+                    }
+                }
+            }
+            if (!selectedItem) {
+                props.onChange(props.items[0].value);
+            }
+
+        }
+        if (e.key === "Enter" || e.key === "Escape") {
+            setActive(false)
+        }
+    }
+
     return (
-        <div>
+        <>
+            <div className={style.select} onKeyUp={onKeyUp} tabIndex={0}>
+                <span className={style.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+                {
+                    active &&
+                    <div className={style.items}>
+                        {props.items.map((el, index) =>
+                            <div
+                                onMouseEnter={()=>{setHoveredElementValue(el.value)}}
+                                className={style.item + " " + (hoveredItem === el ? style.selected : "")}
+                                key={el.value}
+                                onClick={() => {
+                                    onItemClick(el.value)
+                                }}
+                            >{el.title}
+                            </div>)}
+                    </div>
+                }
 
-            {/*filter или find*/}
-            <div>{}</div>
+            </div>
+        </>
 
-            {props.items.map((el,index) => <div>{el.title}</div>)}
-        </div>
     );
 };
 
